b5f44daacb6e7c5cba8d59af5ba508aa1d2d60cb
Vikas_Mhaske <vtmhaske@gmail.com>
2026-04-19 11:21:46 +0530

    adding service layer

--- VikiCarWash.API/Controllers/CarWashBookingController.cs
+++ VikiCarWash.API/Controllers/CarWashBookingController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VikiCarWash.Application.DTOs;
 using VikiCarWash.Application.Interfaces;
+using VikiCarWash.Application.Services;
 using VikiCarWash.Domain.Entities;
 
 namespace VikiCarWash.API.Controllers
@@ -9,91 +10,54 @@ namespace VikiCarWash.API.Controllers
     [Route("api/[controller]")]
     public class CarWashBookingController : ControllerBase
     {
-        private readonly ICarWashBookingRepository _repository;
-        public CarWashBookingController(ICarWashBookingRepository repository)
+        private readonly ICarWashBookingService _service;
+
+        public CarWashBookingController(ICarWashBookingService service)
         {
-            _repository = repository;
+            _service = service;
         }
+
         //GET: api/CarWashBooking
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var data = await _repository.GetAllAsync();
-            var result = data.Select(x => new BookingResponseDTO
-            {
-                Id = x.Id,
-                CustomerName = x.CustomerName,
-                PhoneNumber = x.PhoneNumber,
-                CarType = x.CarType,
-                BookingDate = x.BookingDate,
-                Price = x.Price,
-                IsCompleted = x.IsCompleted
-            });
-            return Ok(result);
+            var data = await _service.GetAllAsync();
+            return Ok(data);
         }
         //GET: api/CarWashBooking/{id}
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            var x = await _repository.GetByIdAsync(id);
-            if (x == null)
+            var data = await _service.GetByIdAsync(id);
+            if (data == null)
             {
                 return NotFound();
             }
-            var result = new BookingResponseDTO
-            {
-                Id = x.Id,
-                CustomerName = x.CustomerName,
-                PhoneNumber = x.PhoneNumber,
-                CarType = x.CarType,
-                BookingDate = x.BookingDate,
-                Price = x.Price,
-                IsCompleted = x.IsCompleted
-            };
-            return Ok(result);
+            
+            return Ok(data);
         }
 
         //POST: api/CarWashBooking
         [HttpPost]
         public async Task<IActionResult> Create(CreateBookingDTO dto)
         {
-            var booking = new CarWashBooking
-            {
-                CustomerName = dto.CustomerName,
-                PhoneNumber = dto.PhoneNumber,
-                CarType = dto.CarType,
-                BookingDate = dto.BookingDate,
-                Price = dto.Price,
-                IsCompleted = false
-            };
-            await _repository.AddAsync(booking);
-            return Ok(booking);
+            var result = await _service.CreateAsync(dto);
+
+            return Ok(result);
         }
 
         //PUT: api/CarWashBooking/{id}
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(int id, UpdateBookingDTO dto)
         {
-            var booking = new CarWashBooking
-            {
-                Id = id,
-                CustomerName = dto.CustomerName,
-                PhoneNumber = dto.PhoneNumber,
-                CarType = dto.CarType,
-                BookingDate = dto.BookingDate,
-                Price = dto.Price,
-                IsCompleted = dto.IsCompleted
-            };
-
-            await _repository.UpdateAsync(booking);
-
-            return Ok(booking);
+            var result = await _service.UpdateAsync(id, dto);
+            return Ok(result);
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            await _repository.DeleteAsync(id);
+           await _service.DeleteAsync(id);
             return Ok();
         }
     }
--- VikiCarWash.API/Program.cs
+++ VikiCarWash.API/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using VikiCarWash.Application.Interfaces;
 using VikiCarWash.Infrastructure.Data;
 using VikiCarWash.Infrastructure.Repositories;
+using VikiCarWash.Application.Services;
+using VikiCarWash.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<ICarWashBookingRepository, CarWashBookingRepository>();
+builder.Services.AddScoped<ICarWashBookingService, CarWashBookingService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
