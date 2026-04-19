4c6ee33b5f0bf0765cc102edfe4c61c35390fd8b
Vikas_Mhaske <vtmhaske@gmail.com>
2026-04-19 11:12:13 +0530

    implemeted DTO

--- VikiCarWash.API/Controllers/CarWashBookingController.cs
+++ VikiCarWash.API/Controllers/CarWashBookingController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VikiCarWash.Application.DTOs;
 using VikiCarWash.Application.Interfaces;
 using VikiCarWash.Domain.Entities;
 
@@ -6,7 +7,7 @@ namespace VikiCarWash.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CarWashBookingController : Controller
+    public class CarWashBookingController : ControllerBase
     {
         private readonly ICarWashBookingRepository _repository;
         public CarWashBookingController(ICarWashBookingRepository repository)
@@ -18,39 +19,77 @@ namespace VikiCarWash.API.Controllers
         public async Task<IActionResult> GetAll()
         {
             var data = await _repository.GetAllAsync();
-            return Ok(data);
+            var result = data.Select(x => new BookingResponseDTO
+            {
+                Id = x.Id,
+                CustomerName = x.CustomerName,
+                PhoneNumber = x.PhoneNumber,
+                CarType = x.CarType,
+                BookingDate = x.BookingDate,
+                Price = x.Price,
+                IsCompleted = x.IsCompleted
+            });
+            return Ok(result);
         }
         //GET: api/CarWashBooking/{id}
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            var booking = await _repository.GetByIdAsync(id);
-            if (booking == null)
+            var x = await _repository.GetByIdAsync(id);
+            if (x == null)
             {
                 return NotFound();
             }
-            return Ok(booking);
+            var result = new BookingResponseDTO
+            {
+                Id = x.Id,
+                CustomerName = x.CustomerName,
+                PhoneNumber = x.PhoneNumber,
+                CarType = x.CarType,
+                BookingDate = x.BookingDate,
+                Price = x.Price,
+                IsCompleted = x.IsCompleted
+            };
+            return Ok(result);
         }
 
         //POST: api/CarWashBooking
         [HttpPost]
-        public async Task<IActionResult> Create(CarWashBooking booking)
+        public async Task<IActionResult> Create(CreateBookingDTO dto)
         {
+            var booking = new CarWashBooking
+            {
+                CustomerName = dto.CustomerName,
+                PhoneNumber = dto.PhoneNumber,
+                CarType = dto.CarType,
+                BookingDate = dto.BookingDate,
+                Price = dto.Price,
+                IsCompleted = false
+            };
             await _repository.AddAsync(booking);
             return Ok(booking);
         }
 
         //PUT: api/CarWashBooking/{id}
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id, CarWashBooking booking)
+        public async Task<IActionResult> Update(int id, UpdateBookingDTO dto)
         {
-            if (id != booking.Id)
+            var booking = new CarWashBooking
             {
-                return BadRequest();
-            }
+                Id = id,
+                CustomerName = dto.CustomerName,
+                PhoneNumber = dto.PhoneNumber,
+                CarType = dto.CarType,
+                BookingDate = dto.BookingDate,
+                Price = dto.Price,
+                IsCompleted = dto.IsCompleted
+            };
+
             await _repository.UpdateAsync(booking);
+
             return Ok(booking);
         }
+
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
--- VikiCarWash.Domain/Entities/CarWashBooking.cs
+++ VikiCarWash.Domain/Entities/CarWashBooking.cs
@@ -9,7 +9,7 @@ namespace VikiCarWash.Domain.Entities
         public int Id {get;set; }
         public string CustomerName { get; set; }
         public string PhoneNumber { get; set; }
-        public string Cartype { get; set; }
+        public string CarType { get; set; }
         public DateTime BookingDate { get; set; }
         public decimal Price { get; set; }
         public bool IsCompleted { get; set; }
