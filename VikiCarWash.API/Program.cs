6e291e93013dbef1761ed681f43c803964932a00
Vikas_Mhaske <vtmhaske@gmail.com>
2026-04-19 12:28:17 +0530

    adding validations

--- VikiCarWash.API/Program.cs
+++ VikiCarWash.API/Program.cs
@@ -7,6 +7,9 @@ using VikiCarWash.Infrastructure.Data;
 using VikiCarWash.Infrastructure.Repositories;
 using VikiCarWash.Infrastructure.Services;
 using VikiCarWash.API.Middleware;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using VikiCarWash.Application.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +23,8 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<ICarWashBookingRepository, CarWashBookingRepository>();
 builder.Services.AddScoped<ICarWashBookingService, CarWashBookingService>();
 builder.Services.AddAutoMapper(cfg => { }, typeof(MappingProfile));
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateBookingValidator>();
 
 var app = builder.Build();
 
---
+++ VikiCarWash.Application/Validators/CreateBookingValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using VikiCarWash.Application.DTOs;
+
+namespace VikiCarWash.Application.Validators;
+
+public class CreateBookingValidator : AbstractValidator<CreateBookingDTO>
+{
+    public CreateBookingValidator()
+    {
+        RuleFor(x => x.CustomerName)
+            .NotEmpty().WithMessage("Customer name is required")
+            .MaximumLength(50);
+
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("Phone number is required")
+            .Length(10).WithMessage("Phone number must be 10 digits");
+
+        RuleFor(x => x.CarType)
+            .NotEmpty().WithMessage("Car type is required");
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than 0");
+
+        RuleFor(x => x.BookingDate)
+            .GreaterThan(DateTime.Now.AddDays(-1))
+            .WithMessage("Booking date must be today or future");
+    }
+}
